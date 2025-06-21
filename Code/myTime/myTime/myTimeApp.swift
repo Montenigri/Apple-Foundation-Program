3ddfa3c91142712bf771232f91b5c90e72ea3fb8
Gabriele Musso <gabrielemusso@live.it>
2025-06-21 10:36:39 +0200

    Creazione progetto in swift

---
+++ Code/myTime/myTime/myTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  myTimeApp.swift
+//  myTime
+//
+//  Created by Gabriele Musso on 21/06/25.
+//
+
+import SwiftUI
+
+@main
+struct myTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
